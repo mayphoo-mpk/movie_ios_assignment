4f0233ef75ddbc70892301143a1ce06b1accd55b
May Phoo <mayphookhant@cbbank.com.mm>
2020-07-16 10:46:49 +0630

    first commit

---
+++ movie_assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie_assignment
+//
+//  Created by May Phoo on 16/07/2020.
+//  Copyright © 2020 padcios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
